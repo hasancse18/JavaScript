561e106e06971a1dcc17c5afecd166e9782fca10
hasancse18 <hasan7779296@gmail.com>
2024-09-03 12:01:10 +0600

    Learning Function in Javascript

---
+++ function.js
@@ -0,0 +1,23 @@
+function isEven(a)
+{
+    return a%2===0? "Even" : "Odd";
+}
+
+console.log(`The number is ${isEven(11)}`);
+
+function add(a,b)
+{
+    return a+b;
+}
+
+function subtraction(a,b)
+{
+    return a-b;
+}
+
+function multiply(a,b)
+{
+    return a*b;
+}
+
+console.log(`The Addition ${add(2,3)}`)
